73ad662886fc88016947e2037324cac7989c5e38
MEG HOWE <howem9@gmail.com>
2021-07-20 11:39:15 -0400

    Added index and create options, need to resolve axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <button v-on:click="addMovie()">addMovie</button>
+    <div v-for="movie in movies" :key="movie.id">
+      <h1>{{ movie.name }}</h1>
+      <h2>{{ movie.year }}</h2>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      movies: [],
+    };
   },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All movies", this.movies);
+      });
+    },
+    addMovie: function () {
+      console.log("Add a movie");
+
+      var params = {
+        name: "The Count of Monte Cristo",
+        year: 1692,
+      };
+      axios.post("http://localhost:3000/movies", params).then((response) => {
+        console.log("Nice", response.data);
+        this.products.push(response.data);
+      });
+    },
+  }
 };
 </script>
