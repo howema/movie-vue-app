2dceb406612f3ef8fd2e03ebc9b01b8a8cbe7b67
MEG HOWE <howem9@gmail.com>
2021-07-27 20:22:53 -0400

    Adds cards

--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="moviesindex">
     <h1>All Monster Movies:</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <div class="card mb-3" style="max-width: 540px">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="actor.png" class="img-fluid rounded-start" alt="card image capppp" />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{{ movie.title }} ({{ movie.year }})</h5>
+              <p class="card-text">
+              {{ movie.plot }}
+              </p>
+              <!-- <p></p>
+              <p>director: {{ movie.director }}</p> -->
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <p></p>
     <p></p>
     <p></p>
